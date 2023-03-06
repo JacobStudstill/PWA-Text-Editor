bd565b579ae11dc0770381be0afd868be88c59b2
Jacob Studstill <jacob.studstill@gmail.com>
2023-03-06 18:23:24 -0500

    Wrote the two functions for getDb and putDb

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -12,10 +12,32 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log("The data is now in the database", result.value)
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+  console.error('getDb was not implemented');
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  result
+    ? console.log("The data was retrieved from the database", result.value)
+    : console.log("The data was not found in the database");
+  return result?.value;
+}
+
+
+
 
 initdb();
